8e0bb676b8819f28bb239f6eba096629a9547385
vnaranjo161 <vnaranjo161@gmail.com>
2024-04-10 22:29:48 -0500

    Inicio login (interfaz, controlador)

--- src/main/java/co/edu/uniquindio/banco/controlador/LoginControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/LoginControlador.java
@@ -1,10 +1,64 @@
 package co.edu.uniquindio.banco.controlador;
 
+import co.edu.uniquindio.banco.modelo.Banco;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
 /**
  * Clase que representa el controlador de la vista de login
  * @author caflorezvi
  */
 public class LoginControlador {
+    @FXML
+    TextField txtIdentificacion;
+    @FXML
+    PasswordField txtPassword;
+
+    Banco banco = Banco.getInstancia();
+    public void iniciarSesion(ActionEvent actionEvent){
+        if (banco.validarUsuario(txtIdentificacion.getText(), txtPassword.getText()) != null){
+            mostrarVentana();
+        }else{
+            crearAlerta("Datos incorrectos, intente nuevamente", Alert.AlertType.ERROR);
+        }
+    }
+
+    public void crearAlerta(String mensaje, Alert.AlertType tipo){
+        Alert alert = new Alert(tipo);
+        alert.setTitle("Alerta");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
 
 
+    public void mostrarVentana(){
+        try {
+            // Cargar la vista
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/panelCliente.fxml"));
+            Parent root = loader.load();
+
+            // Crear la escena
+            Scene scene = new Scene(root);
+
+            // Crear un nuevo escenario (ventana)
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setTitle("Panel cliente");
+
+            // Mostrar la nueva ventana
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
