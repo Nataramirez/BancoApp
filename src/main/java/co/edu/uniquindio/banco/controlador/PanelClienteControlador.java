df7afdd3dfde2a78e276f42bde2eb53c5c7d851a
vnaranjo161 <vnaranjo161@gmail.com>
2024-04-13 11:50:03 -0500

    Actualizar (interfaz, controlador)

--- src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
@@ -1,9 +1,46 @@
 package co.edu.uniquindio.banco.controlador;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 
 /**
  * Clase que se encarga de gestionar las acciones de la interfaz gráfica del panel del cliente.
  * @author caflorezvi
  */
 public class PanelClienteControlador {
+    @FXML
+    private Button btnActualizar;
 
+    public void actualizar(ActionEvent actionEvent){
+        navegarVentana("/actualizar.fxml", "Actualizar datos");
+    }
+
+    public void navegarVentana(String nombreArchivoFxml, String tituloVentana) {
+        try {
+
+            // Cargar la vista
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(nombreArchivoFxml));
+            Parent root = loader.load();
+
+            // Crear la escena
+            Scene scene = new Scene(root);
+
+            // Crear un nuevo escenario (ventana)
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.setTitle(tituloVentana);
+
+            // Mostrar la nueva ventana
+            stage.show();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
